e0c6db6820fc5611309b1061c18fcece171f5ef2
Ivor A Niggebrugge <adam.niggebrugge@gmail.comgit config --global user.email adam.niggebrugge@gmail.com>
2021-08-21 22:28:39 -0400

    20210821 22:28EST set up of files

--- index.js
+++ index.js
@@ -1,7 +1,40 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of this Project?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please provide a brief description of the Project?',
+    },
+    // {
+    //     type: 'input',
+    //     name: 'install',
+    //     message: 'Please provide instructions for installation of your Project? (if applicable)',
+    // },
+    // {
+    //     type: 'input',
+    //     name: 'usage',
+    //     message: 'Please provide usage information about this Project.',
+    // },
+    // {
+    //     type: 'input',
+    //     name: 'contribute',
+    //     message: 'Please provide guidelines for contribution.',
+    // },
+    // {
+    //     type: 'input',
+    //     name: 'test',
+    //     message: 'Please provide test instructions for this Projecct.',
+    // }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
