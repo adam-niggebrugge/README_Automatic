b383b51b029aa0b5deb596a9139300b4504fe2cc
Ivor A Niggebrugge <adam.niggebrugge@gmail.comgit config --global user.email adam.niggebrugge@gmail.com>
2021-08-22 18:07:05 -0400

    20210822 18:06EST added checklist to inquirer questions

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -3,7 +3,10 @@
 function renderLicenseBadge(license) {
   console.log('inside render License Badge');
   console.log(license);
-
+  const appacheBadge = '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
+  const gnuBadge = '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)';
+  const UnlicenseBadge = '[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)';
+  
 }
 
 // TODO: Create a function that returns the license link
